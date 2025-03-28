3589de14e35af10fc72502296be10f6304e49e57
C L <leungc22@gmail.com>
2025-03-27 21:55:26 -0400

    initial commit

---
+++ src/script.js
@@ -0,0 +1,78 @@
+// Simulate Data Fetching Using Promises
+function fetchUserProfile(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("user");
+        }, 1000);
+      });
+    }
+
+function fetchUserPosts(){
+return new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(["my first post", "my second post", "my last post"]);
+    }, 1000);
+    });
+}
+
+function fetchUserComments(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() >= 0.5) {
+                resolve(["good job", "thank you"]);
+            }
+            else {
+                reject("error");
+            }}, 1000);
+        });
+}
+
+// Implement Sequential and Parallel Data Fetching // Try/Catch for Error Handling // Chaining Async Functions
+async function getUserContent() {
+    try{
+        const profile = await fetchUserProfile();
+        console.log('fetching Profile...', profile);
+        const posts = await fetchUserPosts();
+        console.log('fetching Posts...', posts);
+        const comments = await fetchUserComments();
+        console.log('fetching Comments...', comments);
+    } catch (error) {
+        console.error('Error fetching data', error);
+    }
+}
+
+// Refactor with Async/Await
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function userProfile() {
+    console.log('fetching Profile');
+    await delay(1000);
+    return { name: 'user' };
+}
+
+async function userPosts() {
+    console.log('fetching Posts'); 
+    await delay(1000);
+    return ['post 1','post 2','post 3'];
+}
+
+async function userComments() {
+    console.log('fetching Comments');
+    await delay(1000);
+    return ['comment 1','comment 2','comment 3'];
+}
+
+userProfile().then(profile => {
+    console.log(profile);
+  });
+  userPosts().then(posts => {
+    console.log(posts);
+  });
+  userComments().then(comments => {
+    console.log(comments);
+  });
+  
+// Implement Sequential and Parallel Data Fetching // Try/Catch for Error Handling // Chaining Async Functions
+getUserContent();
